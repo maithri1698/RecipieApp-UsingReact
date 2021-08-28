3170f0ebe52034535769d1b6d707a54f19030f6c
maithril698 <maithrilgowda15@gmail.com>
2021-08-28 19:01:34 +0530

    Third Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import Axios from "axios";
+import React, {useState} from "react";
+import RecipeTile from './RecipeTile';
 
 function App() {
+  const [query, setquery] = useState("")
+  const [recipes, setrecipes] = useState([])
+  const [healthLabel, sethealthLabel] = useState("vegan")
+  const YOUR_APP_ID = "7d4bf52d";
+  const YOUR_APP_KEY = "eab923f1a73921f70ef450c533dccb71";
+  var url= `https://api.edamam.com/search?q=${query}&
+  app_id=${YOUR_APP_ID}&
+  app_key=${YOUR_APP_KEY}&health=alcohol-free`;
+
+  async function getRecipies(){
+    var result = await Axios.get(url);
+    setrecipes(result.data.hits);
+    console.log(result.data);
+  }
+  const onSubmit = (e) => {
+    e.preventDefault();
+    getRecipies();
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Food Recipies Plaza 🍔</h1>
+      <form className="app__searchForm" onSubmit={onSubmit}>
+      <input 
+      type="text" 
+      className = "app__input"
+      placeholder="Enter Input" 
+      value={query} 
+      onChange={(e) => setquery(e.target.value)} />
+      <input type="submit" className="app__submit" value="Search" />
+      <select className="app_healthLabels">
+        <option onClick={() => sethealthLabel("vegan")}>Vegan</option>
+      </select>
+      </form>
+      <div className="app__recipes">
+      {recipes.map((recipe) => {
+        return <RecipeTile recipe={recipe} />;
+      })}
+      </div>
     </div>
   );
 }
-
 export default App;
---
+++ src/key.js
@@ -0,0 +1,2 @@
+const YOUR_APP_ID = "7d4bf52d";
+const YOUR_APP_KEY = "eab923f1a73921f70ef450c533dccb71";
